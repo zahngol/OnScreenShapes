9e1d83ae5696295396426f812a243c92e293f05f
Zach <zahngol@gmail.com>
2019-08-26 22:50:44 -0400

    Initial commit of Unity data

---
+++ Assets/Scripts/RotateShape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateShape : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+	private bool isPointerDown;
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		isPointerDown = true;
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		isPointerDown = false;
+	}
+
+	public void OnDrag(PointerEventData data)
+	{
+		if (isPointerDown)
+		{
+			float angle = Mathf.Atan2(data.position.y-transform.parent.position.y, data.position.x-transform.parent.position.x) * Mathf.Rad2Deg;
+			Vector3 temp = new Vector3 (
+				transform.parent.rotation.eulerAngles.x, 
+				transform.parent.rotation.eulerAngles.y, 
+				angle-90);
+			transform.parent.rotation = Quaternion.Euler(temp);
+		}
+	}
+}
---
+++ Assets/Scripts/ScaleShape.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScaleShape : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+	public enum Corner
+	{
+		topRight,
+		topLeft,
+		bottomRight,
+		bottomLeft
+	}
+
+	public Corner corner;
+	
+	private bool isPointerDown;
+
+	private RectTransform shapeRectTransform;
+
+	private Vector2 originalPosition;
+	
+	private void Start()
+	{
+		shapeRectTransform = transform.parent.parent.GetComponent<RectTransform>();
+		Debug.Log(shapeRectTransform.rect);
+		Debug.Log(shapeRectTransform.position);
+		Debug.Log(shapeRectTransform.anchoredPosition);
+	}
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		isPointerDown = true;
+		originalPosition = shapeRectTransform.anchoredPosition;
+		switch (corner)
+		{
+			case Corner.topLeft:
+				shapeRectTransform.pivot = new Vector2(1f,0f);
+				shapeRectTransform.anchoredPosition = new Vector2(
+					shapeRectTransform.anchoredPosition.x + shapeRectTransform.rect.width / 2, 
+					shapeRectTransform.anchoredPosition.y - shapeRectTransform.rect.height / 2);
+				break;
+			case Corner.topRight:
+				shapeRectTransform.pivot = new Vector2(1f,1f);
+				shapeRectTransform.anchoredPosition = new Vector2(
+					shapeRectTransform.anchoredPosition.x + shapeRectTransform.rect.width / 2, 
+					shapeRectTransform.anchoredPosition.y + shapeRectTransform.rect.height / 2);
+				break;
+			case Corner.bottomLeft:
+				shapeRectTransform.pivot = new Vector2(0,0);
+				shapeRectTransform.anchoredPosition = new Vector2(
+					shapeRectTransform.anchoredPosition.x - shapeRectTransform.rect.width / 2, 
+					shapeRectTransform.anchoredPosition.y - shapeRectTransform.rect.height / 2);
+				break;
+			case Corner.bottomRight:
+				shapeRectTransform.pivot = new Vector2(0f, 1f);
+				shapeRectTransform.anchoredPosition = new Vector2(
+					shapeRectTransform.anchoredPosition.x - shapeRectTransform.rect.width / 2, 
+					shapeRectTransform.anchoredPosition.y + shapeRectTransform.rect.height / 2);
+				break;
+		}
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		shapeRectTransform.pivot = new Vector2(0.5f, 0.5f);
+		shapeRectTransform.anchoredPosition = originalPosition;
+		isPointerDown = false;
+	}
+
+	public void OnDrag(PointerEventData data)
+	{
+		if (isPointerDown)
+		{
+			
+		}
+	}
+}
